629db226531ef623589655f52a6cb6477965a4b5
anahojan <89191228@student.upr.si>
2022-10-29 15:32:02 +0200

    modified files

--- DOGGO-project/base/views.py
+++ DOGGO-project/base/views.py
@@ -1,10 +1,22 @@
-from urllib import request
 from django.shortcuts import render
-from django.http import HttpResponse
 
+rooms = [
+    {'id': 1, 'name': 'Lets find a perfect dog for you!'},
+    {'id': 2, 'name': 'Lets meet up!'},
+    {'id': 3, 'name': 'Find a dogwalker'},
+    {'id': 4, 'name': 'Find a breeder!'},
+    {'id': 5, 'name': 'Doggo destress!'},
+]
 
 def home(request):
-    return HttpResponse('Home Page')
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context),
 
-def room(request):
-    return HttpResponse('Room')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+
+    return render(request, 'base/room.html', context)
