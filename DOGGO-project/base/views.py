5bad58f65910b3ae03872769b44b80e2bc00f522
anahojan <89191228@student.upr.si>
2022-11-04 18:56:53 +0100

    typo

--- DOGGO-project/base/views.py
+++ DOGGO-project/base/views.py
@@ -10,7 +10,7 @@ rooms = [
 
 def home(request):
     context = {'rooms': rooms}
-    return render(request, 'base/home.html', context),
+    return render(request, 'base/home.html', context)
 
 def room(request, pk):
     room = None
